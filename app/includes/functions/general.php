02de9b27b5c7633f0e82c740624f61f604236dd7
Bobby <rob.davies1991@gmail.com>
2024-08-29 22:15:58 +0100

    Delete app directory

--- app/includes/functions/general.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-function clean_data($input) {
-    $input = @strip_tags($input);
-    $input = @stripslashes($input);
-    return $input;
-}
-
-?>
--- app/includes/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<div id="footer" class="container">
-    Boobyrae Woz here
-</div>
-</body>
-</html>
